54e7e2b412485d35027c2d6d0f7f370223cc25be
EDDQD <eddeza@gmail.com>
2021-06-22 13:27:22 -0300

    Reacomodado el entry point (app.js)

--- src/app.js
+++ src/app.js
@@ -1,43 +1,45 @@
+// Módulos
 const express = require('express');
 const path = require('path');
-const app = express();
-const method = require('method-override');
+const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
 
-
-app.use(method('_method'));
-app.set("view engine", "ejs")
-app.set('views','./src/views')
+// Configuración de Express
+const app = express();
+app.use(express.static("../public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-const publicpath = path.resolve(__dirname, '../public');
-
 app.use(cookieParser());
+app.use(methodOverride('_method'));
+app.set("view engine", "ejs")
+
+/* No hacía falta esto porque ahora está en el mismo directorio.
+app.set('views','./src/views')*/
+
+/* Esto no debería hacer falta siendo que se puede pasar la ruta relativa.
+const publicpath = path.resolve(__dirname, '../public');
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(publicpath));*/
 
-app.use(express.static(publicpath));
+// Rutas
+const aboutRoutes = require('./routes/aboutRoutes');
+const mainRoutes = require('./routes/mainRoutes');
+const bookingRoutes = require('./routes/bookingRoutes');
+const userRoutes = require ('./routes/usersRoutes');
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en el puerto 3000')
-});
+app.use('/about', aboutRoutes);
+app.use('/main', mainRoutes);
+app.use('/booking', bookingRoutes);
+app.use('/user', userRoutes);
+
 app.get('/', (req,res) => {
     res.render('index', {
         tittle:'productos'
     });
 });
 
+app.listen(3000, () => {
+    console.log('Servidor corriendo en el puerto 3000')
+});
 
-const aboutRoutes = require('./routes/aboutRoutes');
-app.use('/about', aboutRoutes);
-
-const mainRoutes = require('./routes/mainRoutes');
-app.use('/main', mainRoutes);
-
-const bookingRoutes = require('./routes/bookingRoutes');
-app.use('/booking', bookingRoutes);
-
-const userRoutes = require ('./routes/usersRoutes');
-app.use('/user', userRoutes);
-
-
-
+// Falta crear y agregar el 404 acá.

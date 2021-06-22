31289d65ef1e08e479c1718d49ab654fb1e6dc3d
EDDQD <eddeza@gmail.com>
2021-06-22 16:28:07 -0300

    Se movió partials a views y se renombró la carpeta footer a about.

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(methodOverride('_method'));
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 app.set('views','./src/views');
 
 // Módulos de rutas
